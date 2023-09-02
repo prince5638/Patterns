56316c7d7d9e184e8f5c3f4b3a356734ffef2102
Prince Changani <princechangani56385638@gmail.com>
2023-09-02 22:58:00 +0530

    Day-3 Pattern-3 2/09/2023 Triangle Pattern for numbers
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

---
+++ Pattern_3.cpp
@@ -0,0 +1,26 @@
+// 1
+// 1 2
+// 1 2 3 
+// 1 2 3 4
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            /* code */
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+    
+    return 0;
+}
