63afd88d59c143fc67edb4088f1992ad82dbbd58
Prince Changani <princechangani56385638@gmail.com>
2023-09-10 10:10:16 +0530

    Day-7 Pattern-8 10/09/2023 Reverse Star Pyramid
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

---
+++ Pattern_8.cpp
@@ -0,0 +1,31 @@
+// * * * * * * *
+//   * * * * *
+//     * * *
+//       *
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+
+    cout<<"Enter the number's of rows: ";
+    cin>>n;
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout<<" ";
+        }
+        for (int k = 0; k < ((2*n)-(2*i+1)); k++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    
+    return 0;
+}
